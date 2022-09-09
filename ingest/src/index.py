406cf746a977800af70799fc133995afc39a9081
Corey Cole <3700879+corey-cole@users.noreply.github.com>
2022-09-09 08:50:17 -0700

    Use tox in the GH Action for real

--- ingest/src/index.py
+++ ingest/src/index.py
@@ -2,6 +2,8 @@
 from typing import Any, Dict
 
 def handler(event: Dict[str, Any], context: object) -> Dict[str, Any]:
+    # If context isn't used, delete it
+    del context
     return {
         'status': 200,
         'message': 'OK'
